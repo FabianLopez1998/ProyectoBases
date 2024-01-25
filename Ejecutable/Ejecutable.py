1417febfb391228fd7e4fe93787d01bc31019784
FabianLopez199 <lopezfa1998@hotmail.com>
2024-01-25 11:14:13 -0500

    Probar

--- Ejecutable/Ejecutable.py
+++ Ejecutable/Ejecutable.py
@@ -4,4 +4,5 @@ from Controlador.ControladorPrincipal import ControladorPrincipal
 if __name__=="__main__":
     controlador = ControladorPrincipal()
     controlador.Iniciar()
+    #cambio realizado
     #app.exec()
